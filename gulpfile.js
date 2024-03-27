99a65ac8017851167740d247466b10f94e9a33ce
marcochavezco <machavezco@gmail.com>
2024-03-26 19:58:59 -0600

    Initialize modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 // Initialize modules
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const concat = require('concat');
+const postcss = require('postcss');
+const replace = require('replace');
+const sass = require('sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 // File path variables
 
