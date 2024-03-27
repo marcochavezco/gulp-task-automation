cdca2e7df6afab3a0271d586173ed42bef5b6d18
marcochavezco <machavezco@gmail.com>
2024-03-26 23:11:01 -0600

    Add sass, js and cache busting task

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,34 @@ const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
 // File path variables
+const files = {
+  scssPath: 'app/scss/**/*.scss',
+  jsPath: 'app/js/**/*.js',
+};
 
 // Sass task
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss[(autoprefixer(), cssnano())])
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist'));
+}
 
 // Js task
+function jsTask() {
+  return src(files.jsPath)
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'));
+}
 
 // Cachebusting task
+const cbString = new Date().getTime();
+function cacheBustTask() {
+  return src(['index.html']).pipe(replace(/cb=\d+/g, 'cb=' + cbString));
+}
 
 // Watch task
 
